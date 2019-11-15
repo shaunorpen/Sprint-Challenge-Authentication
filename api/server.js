f9a7ffc59a2d9acc985a99a220fac15d26f0dd08
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-15 09:45:47 +0000

    Add middleware

--- api/server.js
+++ api/server.js
@@ -2,15 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const authenticate = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const jokesRouter = require("../jokes/jokes-router.js");
 
-const server = express();
 const sessionConfig = {
-  secret: "My secret"
+    name: "LoggedIn",
+    secret: "My secrete",
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: false
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        knex: require("../database/dbConfig"),
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60
+    })
 };
+
+const server = express();
 
 server.use(helmet());
 server.use(cors());
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,17 +19,34 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await users("users")
-      .where({ username })
-      .first();
-  if ((username && password) && user) {
+    .where({ username })
+    .first();
+  if (username && password && user) {
     const result = bcrypt.compareSync(password, user.password);
     if (result) {
+      req.session.user = user;
       res.status(200).json({ message: "Welcome! You're logged in!" });
     } else {
       res.status(401).json({ message: "Invalid password" });
     }
   } else {
-    res.status(400).json({ message: "Please provide a valid username and password" });
+    res
+      .status(400)
+      .json({ message: "Please provide a valid username and password" });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.json({ message: err.message });
+      } else {
+        res.json("Goodbye! You're logged out.");
+      }
+    });
+  } else {
+    res.end();
   }
 });
 
